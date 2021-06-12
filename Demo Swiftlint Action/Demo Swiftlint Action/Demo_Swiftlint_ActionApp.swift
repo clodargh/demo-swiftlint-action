0ff3ca7e7a6ac17b82b473e757c7ebe410ac140d
Clodagh ML <dev@clodargh.io>
2021-06-12 09:33:38 +0100

    Adds default Swift app with SwiftUI, UITests, no CoreData

---
+++ Demo Swiftlint Action/Demo Swiftlint Action/Demo_Swiftlint_ActionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_Swiftlint_ActionApp.swift
+//  Demo Swiftlint Action
+//
+//  Created by Clodagh ML on 12/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct Demo_Swiftlint_ActionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
